d890bdd99ed2d1784c12d6ba67f96eb66525a698
3ken <aekstrand92@gmail.com>
2014-02-13 20:35:17 +0100

    Friskat upp GE och skapat Questions

--- The Quiz Game/src/GameEngine.java
+++ The Quiz Game/src/GameEngine.java
@@ -3,24 +3,31 @@ public class GameEngine {
 	
 	public GameEngine() {
 	
+		GUI gui = new GUI();
+		Questions questions = new Questions();
+		
+	}
+	
+	public static void main(String [ ] args)
+	{
+	      GameEngine gameengine = new GameEngine();
 	}
 
-
-	public void name()
-	{
+	public void name(){
 		System.out.println("Hello World");
 	}
 
-	public void Store()
-	{	
-	//store saker
+	public void Store(){	
+		//store saker
 	}
 
 
-	public void IOSA()
-	{}
+	public void IOSA(){
+		
+	}
 
 
-	public void skriva()
-	{}
+	public void skriva(){
+		
+	}
 }
