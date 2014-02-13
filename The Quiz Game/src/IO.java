d0a8157f1e48628a602db7403206eca5596e9210
3ken <aekstrand92@gmail.com>
2014-02-13 21:46:45 +0100

    lagt till lite kod i IO klassen

--- The Quiz Game/src/IO.java
+++ The Quiz Game/src/IO.java
@@ -1,31 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class IO {
-	private ArrayList<ArrayList<String>> questionFile;
-	
-	/**
-	 * 
-	 */
-	public IO(){
-		
-		questionFile = new ArrayList<ArrayList<String>>();
-		
-		
+	private ArrayList<String> questions;
+	private ArrayList<ArrayList<String>> allQuestions;
+	private BufferedReader br;
+	public IO() {
+		File file = new File("C:\\fragor.txt");
 	}
-	
-	/**
-	 * 
-	 */
-	public ArrayList<ArrayList<String>> getFile(){
-		
-		return questionFile;
-	}
-	
-	/**
-	 * 
-	 */
-	public void putFile(){
-		
+	public ArrayList<ArrayList<String>> readFile() {
+		questions = new ArrayList<String>();
+		allQuestions = new ArrayList<ArrayList<String>>();
+		new ArrayList<String>();
+		try {
+			br = new BufferedReader(new FileReader("C:\\fragor.txt"));
+			String line;
+			int k = 0;
+			while((line = br.readLine()) != null) {
+				if(line.equals("")){
+					@SuppressWarnings("unchecked")
+					ArrayList<String> tempQuest = ((ArrayList<String>) questions.clone());
+					allQuestions.add(k, tempQuest);
+					questions.clear();
+					k++;
+				}
+				else {
+					questions.add(line);
+				}
+			}
+		}catch (IOException e) {
+			System.out.println("ERROR! Error occurd when file was read.");
+		}
+		return allQuestions;
+		//
 	}
 }
+
