18be9507d52b432c4ba0cd077cbef98f6d3b75a3
Simon Andersson <andsimo@student.chalmers.se>
2014-02-14 10:05:50 +0100

    Questions uppdaterad med shuffle som separat metod.

---
+++ The Quiz Game/src/Questions2.java
@@ -0,0 +1,49 @@
+import java.util.Collections;
+import java.util.Random;
+import java.util.ArrayList;
+
+
+public class Questions2 {
+	
+	private ArrayList<ArrayList<String>> originalList;
+	private ArrayList<ArrayList<String>> newList;
+	IO io;
+	
+	
+	
+	public Questions2()
+	{
+	    io = new IO();	//Creates a new instance of IO.
+	}
+	
+	
+	/**
+	 * Get an array from IO, calls shuffle-method. Takes 5 lists and puts them in a new list to be returned. 
+	 */
+	public ArrayList<ArrayList<String>> getQuestions(int x)
+	{
+		newList = new ArrayList<ArrayList<String>>();
+		originalList = io.readFile();
+		shuffle(originalList);			//Shuffles the original List. 
+		
+		for(int i = 0; i < x; i++)
+		{
+			newList.add(originalList.get(i));
+		}
+		return newList;
+		
+	}
+	
+
+	/**
+	 * Shuffles arraylist as parameter with system time as seed.
+	 */
+	private void shuffle(ArrayList<ArrayList<String>> list)
+	{
+		long seed = System.nanoTime();				//New seed for randomizer.
+		Collections.shuffle(list, new Random(seed));	
+	}
+	
+	
+
+}
