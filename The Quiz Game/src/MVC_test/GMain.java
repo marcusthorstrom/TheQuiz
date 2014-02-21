86683dfc2fedd6b57ab031f9aff970bb53483098
marcusthorstrom <marcus@thorstrom.se>
2014-02-21 08:34:16 +0100

    Ändrat addObserver

--- The Quiz Game/src/MVC_test/GMain.java
+++ The Quiz Game/src/MVC_test/GMain.java
@@ -3,7 +3,8 @@ package MVC_test;
 public class GMain {
 	public static void main(String[] args) {
 		GView view = new GView();
-		GModel model = new GModel(view);
+		GModel model = new GModel();
+		model.addObserver(view);
 		GController controller = new GController(view, model);
 		model.playGame();
 	}
