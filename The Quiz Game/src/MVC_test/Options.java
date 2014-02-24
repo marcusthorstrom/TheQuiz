1afe1626dc83d90b4959fb0d800b8a7dc57011d7
vidara <vidara@ju029-15.ju029.studat.a.1.hb.cdal.chalmers.se>
2014-02-24 16:25:28 +0100

    options object vidar

---
+++ The Quiz Game/src/MVC_test/Options.java
@@ -0,0 +1,26 @@
+package MVC_test;
+
+
+
+final public class Options {
+
+	private int volume;
+	private int gamerounds;
+	
+	public Options(){
+		volume = 50;
+		gamerounds = 5;
+	}
+	public int getVolume(){
+		return volume;
+	}
+	public int getGameRounds(){
+		return gamerounds;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	public void setGameRounds(int value) {
+		gamerounds = value;
+	}
+}
