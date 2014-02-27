fbd0ecfedf942644366080a2fa73d6d64e3dc38e
emilri <emilri@ju020-03.ju020.studat.a.1.hb.cdal.chalmers.se>
2014-02-27 17:09:20 +0100

    Client test

--- The Quiz Game/src/Client/ConnectionToServer.java
+++ The Quiz Game/src/Client/ConnectionToServer.java
@@ -1,12 +1,17 @@
 package Client;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
@@ -14,44 +19,48 @@ import Server.SingleQuestion;
 
 public class ConnectionToServer {
 	private static Socket socket;
-	private boolean connection;
-	private InetAddress ip = null;
-	private DataInputStream in;
-	private DataOutputStream out;
-	private byte byteS;
-	private int serviceWrite = 1; //For writing to the server
-	private int serviceRead = 0;  //For reading to the server
-	
+	private InetAddress ip;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+
 	public ConnectionToServer() throws IOException {
-		InetAddress ip;
+		InetAddress ip = null;
 		ip = InetAddress.getByName("localHost");
-			socket = new Socket(ip, 13337);
-			connection = false;
-			
-		DataInputStream in = new DataInputStream(socket.getInputStream());
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		
-		
+		socket = new Socket(ip, 13337);
+
+		out = new ObjectOutputStream(socket.getOutputStream());
+		in = new ObjectInputStream(socket.getInputStream());
+
 	}
-	public void writeQuestion(SingleQuestion qu) {
-		try {
-			
-			out.write(serialize(qu));
+
+	/*
+	 * public void writeQuestion(SingleQuestion qu) { try {
+	 * 
+	 * out.write(serialize(qu)); } cat(SingleQuestion) o.readObject()ch
+	 * (IOException e) { System.out.println("Not able to serialize the object");
+	 * e.printStackTrace(); }
+	 * 
+	 * }
+	 */
+
+
+	public ArrayList<SingleQuestion> getQuestions(int gameRounds){
+		ArrayList<SingleQuestion> sqList = new ArrayList<SingleQuestion>();
+		ArrayList<ArrayList<String>> aal = new ArrayList<ArrayList<String>>();
+		
+		try{
+			out.writeObject(gameRounds);
+			try{
+				aal=(ArrayList<ArrayList<String>>)in.readObject();
+			}catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}
+		}catch(SocketException e){
+			System.out.println(e.toString());
 		} catch (IOException e) {
-			System.out.println("Not able to serialize the object");
 			e.printStackTrace();
 		}
 		
-	}
-	public void getQuestions(int gameRounds) throws IOException {
-		out.writeInt(gameRounds);
-		byteS = in.readByte();
-	}
-
-	public static byte[] serialize(SingleQuestion obj) throws IOException {
-	    ByteArrayOutputStream out = new ByteArrayOutputStream();
-	    ObjectOutputStream os = new ObjectOutputStream(out);
-	    os.writeObject(obj);
-	    return out.toByteArray();
+		return sqList;
 	}
 }
